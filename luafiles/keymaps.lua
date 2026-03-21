4480acb8ccca008b3060f8efe8d615e6a62a5e09
AKC27873 <akc27873@gmail.com>
2026-03-20 20:29:58 -0400

    jk

--- luafiles/keymaps.lua
+++ luafiles/keymaps.lua
@@ -2,6 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 -- Exit insert mode with jk
-vim.keymap.set("i", "kk", "<Esc>", { noremap = true, silent = true })
-vim.keymap.set("v", "kk", "<Esc>", { noremap = true, silent = true })
-vim.keymap.set("s", "kk", "<Esc>", { noremap = true, silent = true })
+vim.keymap.set("i", "jk", "<Esc>", { noremap = true, silent = true })
+vim.keymap.set("v", "jk", "<Esc>", { noremap = true, silent = true })
+vim.keymap.set("s", "jk", "<Esc>", { noremap = true, silent = true })
