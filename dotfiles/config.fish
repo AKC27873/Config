5adb548ee7da816dc0e7b6715626b4b168bb4ef8
akc27873 <akc27873@gmail.com>
2026-09-03 08:44:15 -0400

    adding color

--- dotfiles/config.fish
+++ dotfiles/config.fish
@@ -6,5 +6,13 @@ if status is-interactive
     alias ll='eza -lah --icons'
     alias la='eza -a --icons'
     alias lt='eza --tree --icons'
+    alias grep='rg --color=always'
     set -gx GOPATH /opt/golang
+    # Syntax highlighting
+    set -g fish_color_command green
+    set -g fish_color_param normal
+    set -g fish_color_option cyan
+    set -g fish_color_quote yellow
+    set -g fish_color_error red
+    set -g fish_color_comment brblack
 end
